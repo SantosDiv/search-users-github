901f52e7ac6063929d4a39e597c8f4c24d40ee1b
Diogenes <diogenesfelipe.s@outlook.com>
2021-05-02 15:38:32 -0300

    feat/add page summerjob

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import SummerJob from './pages/SummerJob';
 import './App.css';
 
 class App extends React.Component {
@@ -11,6 +12,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route exact path="/profile/:username" component={ Profile } />
+          <Route path="/summerjobexemple" component={ SummerJob } />
         </Switch>
       </Fragment>
     );
---
+++ src/pages/SummerJob.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function SummerJob() {
+  return (
+    <div>
+      <img src="/img/diagram1.png" alt="Diagram prop drinling"/>
+    </div>
+  );
+}
+
+export default SummerJob;
